0cd1d652a72c97475b46be585466e8f786673a36
Glenn Lefevere <glennlefevere@hotmail.com>
2021-12-25 19:42:13 +0100

    change pwa settings

--- apps/pwa-mfe1/src/app/app.module.ts
+++ apps/pwa-mfe1/src/app/app.module.ts
@@ -14,9 +14,7 @@ import { environment } from '../environments/environment';
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
-      // Register the ServiceWorker as soon as the app is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerImmediately',
     }),
   ],
   providers: [],
--- apps/pwa-shell/src/app/app.module.ts
+++ apps/pwa-shell/src/app/app.module.ts
@@ -1,20 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
+import {AppComponent} from './app.component';
+import {NxWelcomeComponent} from './nx-welcome.component';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {environment} from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
   imports: [BrowserModule, ServiceWorkerModule.register('ngsw-worker.js', {
-  enabled: environment.production,
-  // Register the ServiceWorker as soon as the app is stable
-  // or after 30 seconds (whichever comes first).
-  registrationStrategy: 'registerWhenStable:30000'
-})],
+    enabled: environment.production,
+    registrationStrategy: 'registerImmediately',
+  })],
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
