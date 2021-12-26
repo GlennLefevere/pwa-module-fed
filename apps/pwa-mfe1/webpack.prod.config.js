d4df4c5d6e985694f3ba903d1e05ec28cdea274c
glenn <glenn.lefevere@liantis.be>
2021-12-26 19:51:02 +0100

    mfe wip

--- apps/pwa-mfe1/webpack.prod.config.js
+++ apps/pwa-mfe1/webpack.prod.config.js
@@ -1,1 +1,57 @@
-module.exports = require('./webpack.config');
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const mf = require("@angular-architects/module-federation/webpack");
+const path = require("path");
+const share = mf.share;
+
+const sharedMappings = new mf.SharedMappings();
+sharedMappings.register(
+  path.join(__dirname, '../../tsconfig.base.json'),
+  [/* mapped paths to share */]);
+
+module.exports = {
+  output: {
+    uniqueName: "pwaMFE1",
+    publicPath: "https://pwa-module-fed.herokuapp.com/pwa-mfe1/"
+  },
+  optimization: {
+    runtimeChunk: false
+  },
+  resolve: {
+    alias: {
+      ...sharedMappings.getAliases(),
+    }
+  },
+  experiments: {
+    outputModule: true
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      library: {type: "module"},
+
+      // For remotes (please adjust)
+      name: "pwaMfe",
+      filename: "remoteEntry.js",
+      exposes: {
+        './Module': 'apps/pwa-mfe1/src/modules/flights/flights.module.ts',
+      },
+
+      // For hosts (please adjust)
+      // remotes: {
+      //     "pwaMfe1": "http://localhost:4200/remoteEntry.js",
+      //     "server": "http://localhost:4200/remoteEntry.js",
+
+      // },
+
+      shared: share({
+        "@angular/core": {singleton: true, strictVersion: true, requiredVersion: 'auto'},
+        "@angular/common": {singleton: true, strictVersion: true, requiredVersion: 'auto'},
+        "@angular/common/http": {singleton: true, strictVersion: true, requiredVersion: 'auto'},
+        "@angular/router": {singleton: true, strictVersion: true, requiredVersion: 'auto'},
+
+        ...sharedMappings.getDescriptors()
+      })
+
+    }),
+    sharedMappings.getPlugin()
+  ],
+};
